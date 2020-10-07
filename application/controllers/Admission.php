e184820838690fddfcb4894e9bb7d777d043b3f7
operationdown <56550702+operationdown@users.noreply.github.com>
2020-10-07 11:09:01 +0800

    update admission. add new function

--- application/controllers/Admission.php
+++ application/controllers/Admission.php
@@ -25,4 +25,43 @@ class Admission extends CI_Controller {
 	function submit_apps() {
 		$this->output->enable_profiler(true);
 	}
+	
+		public function seed_apps() {
+
+		$data = array();
+
+		$data['Courses'] = array(
+			'' => 'Please Choose',
+			'Certificate of Competency in Food & Beverage Operations' => 'Certificate of Competency in Food & Beverage Operations',
+			'Certificate of Competency in Fast Food Operations' => 'Certificate of Competency in Fast Food Operations',
+			'Certificate of Competency in Culinary Arts' => 'Certificate of Competency in Culinary Arts',
+			'Certificate of Completion in Retail Operations' => 'Certificate of Completion in Retail Operations',
+			'Certificate of Completion in Wellness Management' => 'Certificate of Completion in Wellness Management',
+			'Certificate of Completion in Accommodation Services' => 'Certificate of Completion in Accommodation Services',
+			'Certificate of Completion in Reception Operations Services' => 'Certificate of Completion in Reception Operations Services',
+			'Certificate of Completion in Barista & Cafe Operation' => 'Certificate of Completion in Barista & Cafe Operations',
+			'Certificate of Completion in Call Centre' => 'Certificate of Completion in Call Centre',
+			'Certificate of Completion in Safety & Security Operations' => 'Certificate of Completion in Safety & Security Operations',
+		);
+
+		$data['gender'] = array(
+			'' => 'Please Choose',
+			'Male' => 'Male',
+			'Female' => 'Female',
+		);
+
+		$data['nationality'] = array(
+			'' => 'Please Choose',
+			'Malaysian' => 'Malaysian',
+			'Others' => 'Others',
+		);
+
+		$data['marital'] = array(
+			'' => 'Please Choose',
+			'Single' => 'Single',
+			'Married' => 'Married',
+		);
+
+		$this->load->view('apps_seed', $data);
+	}
 }
